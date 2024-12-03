bbc4c2efba5e599c6ea10752d0b82c392196be9a
Petar Radosevic <petar@petar.dev>
2024-12-03 12:45:16 +0100

    clean things up a bit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Advent of Code 2024 - Javascript Edition
+
+Behold! The solutions to the Advent of Code 2024 puzzles, done by a JS Novice.
+
+I'm writing the solutions on the Deno runtime, meaning if you want to run them yourself you need to have Deno installed.
+
+## Getting your answers
+
+If you want all the answers, do a `deno -A main.ts`. If you want the answer for single day, do a `deno -A day-01.ts`.
--- main.ts
+++ main.ts
@@ -1,39 +1,6 @@
-
-import { TextLineStream } from "jsr:@std/streams@1.0.8";
-
-// Read lines from a file and return a stream of lines.
-async function readLines(path: string) {
-    const file = await Deno.open(path);
-    const lineStream = file.readable
-        .pipeThrough(new TextDecoderStream())
-        .pipeThrough(new TextLineStream());
-
-    return lineStream;
-}
-
-async function readAndSortInput(path: string) {
-    const lines = await readLines(path);
-
-    const left_lines = [];
-    const right_lines = [];
-    for await (const line of lines) {
-        const [left, right] = line.split(/\s+/);
-
-        left_lines.push(left);
-        right_lines.push(right);
-    }
-    left_lines.sort();
-    right_lines.sort();
-
-    return [left_lines, right_lines];
-}
+import { day_01 } from "./day-01.ts";
 
 if (import.meta.main) {
-    const [left, right] = await readAndSortInput("./inputs/day-01.txt");
-
-    let distance = 0;
-    for (let i = 0; i < left.length; i++) {
-        distance += Math.abs(parseInt(left[i]) - parseInt(right[i]));
-    }
-    console.log(distance);
+    const day_01_result = await day_01("./inputs/day-01.txt");
+    console.log(`Day one result: ${day_01_result}`);
 }

1a1df4c636cd3773686df0cd281a8c99416ee147
Petar Radosevic <petar@petar.dev>
2024-12-04 15:45:16 +0100

    int first, then sort

--- aoc_ex/lib/aoc_ex/day01.ex
+++ aoc_ex/lib/aoc_ex/day01.ex
@@ -4,13 +4,13 @@ defmodule AocEx.Day01 do
       AocEx.Utils.stream_input(input, :line)
       |> Stream.map(&String.split/1)
       |> Enum.reduce({[], []}, fn [left, right], {lefts, rights} ->
-        {[left | lefts], [right | rights]}
+        {[String.to_integer(left) | lefts], [String.to_integer(right) | rights]}
       end)
       |> then(fn {l, r} -> {Enum.sort(l), Enum.sort(r)} end)
 
     Enum.zip(left, right)
     |> Enum.map(fn {l, r} ->
-      abs(String.to_integer(l) - String.to_integer(r))
+      abs(l - r)
     end)
     |> Enum.sum()
   end

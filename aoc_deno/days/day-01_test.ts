95b04f74f055e2bca41b69abd02a6db050531937
Petar Radosevic <petar@petar.dev>
2024-12-10 23:04:39 +0100

    tests: fix tests for day one

--- aoc_deno/days/day-01_test.ts
+++ aoc_deno/days/day-01_test.ts
@@ -7,11 +7,11 @@ import {
 } from "./day-01.ts";
 
 Deno.test(async function test_total_distance() {
-    const pair = await readAndSortInput("./inputs/day-01.txt");
-    assertEquals(total_distance(pair), 3574690);
+    const pair = await readAndSortInput("./inputs/day-01.test");
+    assertEquals(total_distance(pair), 11);
 });
 
 Deno.test(async function test_similarity() {
-    const pair = await readAndSortInput("./inputs/day-01.txt");
-    assertEquals(similarity_score(pair), 22565391);
+    const pair = await readAndSortInput("./inputs/day-01.test");
+    assertEquals(similarity_score(pair), 31);
 });
